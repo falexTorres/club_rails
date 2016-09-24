46746db9d76c402a5264292b5698b32b44ea9b5f
falexTorres <rkc870@my.utsa.edu>
2016-09-23 20:46:04 -0500

    reconfigured for postgresql

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 20160918162821) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "abouts", force: :cascade do |t|
     t.string   "title_1"
     t.string   "title_2"
